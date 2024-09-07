fac12b258b466875ab493cf080d1aa75c732c0f1
Meadoughnut <127433011+Meadoughnut@users.noreply.github.com>
2024-09-07 18:53:10 -0400

    About me section got more styling

--- My Portofolio/src/components/AboutMe.jsx
+++ My Portofolio/src/components/AboutMe.jsx
@@ -1,9 +1,14 @@
 // /src/components/AboutMe.jsx
+
+
 const AboutMe = () => {
     return (
       <section>
-        <h2>About Me</h2>
-        <img src="./src/" alt="Meadin's Avatar" className="avatar" />
+        <hr></hr>
+        <h2>I'M  MEADIN  MENBERE</h2>
+       
+        <br></br>
+        {/* <img src="./src/assets" alt="Meadin's Avatar" className="avatar" /> */}
         <p>
         I am Front End Developer with a strong foundation in designing user-centric, responsive web interfaces. Skilled in HTML, CSS, JavaScript, and experienced with frameworks like React. Demonstrated ability to collaborate with cross-functional teams to deliver high-quality web applications, continually enhancing user experience and performance. Committed to leveraging cutting-edge technologies and best practices in web development.
 
